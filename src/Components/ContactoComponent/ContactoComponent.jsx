43587a2ffbb3920a047d1d6670f34f4c08a8a69e
Joaquin Aguirre <joaquinaguirre17@gmail.com>
2024-07-02 19:42:34 -0300

    cambio en letras

--- src/Components/ContactoComponent/ContactoComponent.jsx
+++ src/Components/ContactoComponent/ContactoComponent.jsx
@@ -6,14 +6,14 @@ function ContactoComponent() {
             <h1>Contactame</h1>
             <div className='contacto'>
                 <div className='maps '>
-                    <h4 className='local'>Ubicacion de mi local</h4>
+                    <h4 className='local'>UBICACION DE MI LOCAL</h4>
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3404.141780766186!2d-64.16698292539597!3d-31.437763497283324!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9432a2c48c6d03db%3A0x5591f430c3289a9b!2sAv.%20Revoluci%C3%B3n%20de%20Mayo%201620%2C%20C%C3%B3rdoba!5e0!3m2!1ses-419!2sar!4v1717643840177!5m2!1ses-419!2sar"
                         allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade">
                     </iframe>
                 </div>
 
                 <div className='contact'>
-                    <h4 className='redes'>Mis Redes Sociales</h4>
+                    <h4 className='redes'>REDES SOCIALES</h4>
                     <ul class="social-list">
                         <li>
                             <a href="https://www.instagram.com/lia_nails_manicure?igsh=MTJoOXN0ODdyeHk5YQ==" target="_blank"><i className="fab fa-instagram"></i> </a>
@@ -23,7 +23,7 @@ function ContactoComponent() {
                         </li>
                     </ul>
                     <div className='horario'>
-                        <h4 className='redes'>Mis Horarios</h4>
+                        <h4 className='redes'>HORARIOS</h4>
                         <ul class="horario-list">
                             <li>
                                 <div className="dropdown">
